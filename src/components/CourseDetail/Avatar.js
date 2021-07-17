c31e9809ec896c81c993e0b15fe228524f59928a
Huynh Lam Tu <chanestdevil@gmail.com>
2021-07-17 20:25:28 +0700

    comple course detail banner

--- src/components/CourseDetail/Avatar.js
+++ src/components/CourseDetail/Avatar.js
@@ -1,84 +1,96 @@
-import { Box, Button, makeStyles, Paper, Typography } from '@material-ui/core';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import axios from 'axios';
-import cn from 'classnames';
-import React, { useEffect, useState } from 'react';
-import Moment from 'react-moment';
-import { Link, useParams } from 'react-router-dom';
-import * as env from '../../config/env.config';
+import {
+  Box,
+  Button,
+  makeStyles,
+  Paper,
+  Typography,
+  Container,
+  Grid,
+} from "@material-ui/core";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import axios from "axios";
+import cn from "classnames";
+import React, { useEffect, useState } from "react";
+import Moment from "react-moment";
+import { Link, useParams } from "react-router-dom";
+import * as env from "../../config/env.config";
 const common_fontsize = 18;
 const styles = makeStyles((theme) => ({
   course_detail_wrapper: {},
   ava_course: {
-    position: 'relative'
+    position: "relative",
   },
   section_header: {
     minHeight: 100,
     marginTop: 100,
-    position: 'relative'
+    position: "relative",
   },
   course_name: {
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   course_header_title: {
-    textAlign: 'left',
+    textAlign: "left",
     paddingTop: 12,
     paddingBottom: 12,
-    color: 'black'
+    color: "black",
   },
   section_short_des: {
     minHeight: 100,
-    fontSize: common_fontsize
+    fontSize: common_fontsize,
   },
   des: {
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   section_description: {
     minHeight: 100,
-    fontSize: common_fontsize
+    fontSize: common_fontsize,
   },
   section_syllabus: {
     minHeight: 100,
-    fontSize: common_fontsize
+    fontSize: common_fontsize,
   },
   section_rating: {},
   section_feedback: {
-    marginBottom: 16
+    marginBottom: 16,
   },
   paper: {
     padding: 32,
-    textAlign: 'left',
+    textAlign: "left",
     color: theme.palette.text.secondary,
-    marginBottom: 16
+    marginBottom: 16,
   },
   box_cat: {
     padding: 12,
-    '& .MuiTypography-root': {
-      fontSize: common_fontsize
-    }
+    "& .MuiTypography-root": {
+      fontSize: common_fontsize,
+    },
   },
   btn: {
-    textTransform: 'capitalize'
+    textTransform: "capitalize",
   },
   favo: {
-    position: 'absolute',
-    bottom: 0,
+    position: "absolute",
+    top: 5,
     right: 0,
-    zIndex: '1500!important',
-    color: '#95f0ff',
-    '& .MuiButtonBase-root': {
-      border: 'none!important',
-      color: '#95f0ff'
-    }
-  }
+    zIndex: "1500!important",
+    color: "#95f0ff",
+    "& .MuiButtonBase-root": {
+      border: "none!important",
+      color: "#95f0ff",
+    },
+  },
+  course_banner: {
+    display: "grid",
+    gridTemplateColumns: "2fr 1fr",
+  },
 }));
 
 export default function Avatar({}) {
   const classes = styles();
   const { course_id } = useParams();
   const [course_detail, setcourse_detail] = useState({});
-  const [last_updated, set_last_updated] = useState('');
+  const [last_updated, set_last_updated] = useState("");
   const [user_role, setUser_role] = useState(0);
   const [is_favorite, set_is_favorite] = useState(false);
 
@@ -98,9 +110,9 @@ export default function Avatar({}) {
   const handleFavoriteClick = (e) => {
     const toggle_favorite_url = `${env.DEV_URL}/api/student/toggle-favorite`;
     const data = {
-      user_id: +sessionStorage.getItem('user_login_id'),
+      user_id: +sessionStorage.getItem("user_login_id"),
       course_id: course_id,
-      is_favorite: is_favorite
+      is_favorite: is_favorite,
     };
 
     axios.patch(toggle_favorite_url, data, {}).then((ret) => {
@@ -128,17 +140,18 @@ export default function Avatar({}) {
   useEffect(() => {
     getAva();
 
-    const user_role = sessionStorage.getItem('user_role');
+    const user_role = sessionStorage.getItem("user_role");
     setUser_role(+user_role);
-    getIsFavorite(+sessionStorage.getItem('user_login_id'), course_id);
+    getIsFavorite(+sessionStorage.getItem("user_login_id"), course_id);
   }, [course_id]);
   return (
     <Paper
       style={{
+        height: "300px",
         backgroundImage: `url(${course_detail.course_avatar_url})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
       className={cn(classes.paper, classes.ava_course)}
     >
@@ -150,7 +163,7 @@ export default function Avatar({}) {
               className={cn(classes.link, classes.favo)}
               // to={`/course/${course_id}`}
             >
-              <Button variant='outlined' size='small' color='primary'>
+              <Button variant="outlined" size="small" color="primary">
                 <FavoriteIcon />
               </Button>
             </Link>
@@ -160,28 +173,15 @@ export default function Avatar({}) {
               className={cn(classes.link, classes.favo)}
               // to={`/course/${course_id}`}
             >
-              <Button variant='outlined' size='small' color='primary'>
+              <Button variant="outlined" size="small" color="primary">
                 <FavoriteBorderIcon />
               </Button>
             </Link>
           )}
         </Box>
       ) : (
-        ''
+        ""
       )}
-      <Typography
-        variant='h4'
-        className={cn(classes.course_header_title, classes.course_name)}
-      >
-        {course_detail.course_name}
-      </Typography>
-      <Typography variant='h4' className={classes.course_header_title}>
-        {course_detail.course_title}
-      </Typography>
-      <Typography variant='h6' className={classes.course_header_title}>
-        Last updated - &nbsp;
-        <Moment format='MM/DD/YYYY HH:MM:SS'>{last_updated}</Moment>
-      </Typography>
     </Paper>
   );
 }
